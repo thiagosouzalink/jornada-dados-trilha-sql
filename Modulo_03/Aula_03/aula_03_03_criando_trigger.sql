2f392dc72bdec2c5b6ef01678d4c7b24de8fbb22
Thiago Souza <thiagolsmail@gmail.com>
2026-08-01 18:15:37 -0300

    Aula 3 –  Triggers e Functions

---
+++ Modulo_03/Aula_03/aula_03_03_criando_trigger.sql
@@ -0,0 +1,17 @@
+-- Aula 3 –  Triggers e Functions
+
+CREATE FUNCTION registrar_auditoria_salario()
+RETURNS TRIGGER
+AS $$
+BEGIN
+	INSERT INTO Funcionario_Auditoria (id, salario_antigo, novo_salario)
+    VALUES (OLD.id, OLD.salario, NEW.salario);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_salario_modificado 
+AFTER UPDATE OF salario ON Funcionario
+FOR EACH ROW
+EXECUTE FUNCTION registrar_auditoria_salario();
---
+++ Modulo_03/Aula_03/aula_03_04_testando_trigger_auditoria.sql
@@ -0,0 +1,9 @@
+-- Aula 3 –  Triggers e Functions
+
+SELECT * FROM funcionario;
+
+UPDATE funcionario
+SET salario = 4300
+WHERE nome = 'Ana';
+
+SELECT * FROM funcionario_auditoria;
